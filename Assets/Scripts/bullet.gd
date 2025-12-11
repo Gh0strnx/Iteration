00c45892cc4433f06f0e52c2901c819f0ca464ff
Gh0strnx <lucadamico01@gmail.com>
2025-12-11 13:36:41 +1100

    cannot ermemem

--- Assets/Scripts/bullet.gd
+++ Assets/Scripts/bullet.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
 @export var speed = 720
+@export var bulletBounces = 0
 
 func _physics_process(delta):
 	position += transform.x * speed * delta
+	
 	
 	
 func _on_body_entered(body: Node2D) -> void:
--- Assets/Scripts/gun.gd
+++ Assets/Scripts/gun.gd
@@ -1,53 +1,73 @@
 extends Node2D
 
-@export var bulletAmount = 3
-@export var reloadTime = 3
-@export var attackSpeed = 0.5
+@export var bulletAmount := 3
+@export var reloadTime := 3.0
+@export var attackSpeed := 0.4
+@export var bulletSpread := 0.05
+@export var bulletsShot := 1
+@export var timerSpeed := 0.3
 
-var bulletPrefab = preload("res://Assets/Scenes/bullet.tscn")
-var currentBulletAmount = bulletAmount
+
+var bulletPrefab := preload("res://Assets/Scenes/bullet.tscn")
+var currentBulletAmount := 0
+
+
 
 var can_shoot: bool = true
 var reloading: bool = false
 
 func _ready() -> void:
-	# Shot cooldown timer
+	currentBulletAmount = bulletAmount
+
 	$coolDown.wait_time = attackSpeed
 	$coolDown.one_shot = true
 	$coolDown.connect("timeout", Callable(self, "_on_timer_timeout"))
-	
-	# Reload timer
+
 	$Reload.wait_time = reloadTime
 	$Reload.one_shot = true
 	$Reload.connect("timeout", Callable(self, "_on_reload_timeout"))
 
 func _physics_process(_delta: float) -> void:
-	# Only shoot if not reloading, can shoot, and have bullets
-	if Input.is_action_pressed("Shoot") and can_shoot and not reloading and currentBulletAmount > 0:
-		currentBulletAmount -= 1
-		spawn_bullet()
-		can_shoot = false
-		$coolDown.start()
+	# Fire once per click, not every frame while held
+	if Input.is_action_just_pressed("Shoot") and can_shoot and not reloading and currentBulletAmount > 0:
+		_fire_burst()
 
-	# Start reload once when empty
 	if currentBulletAmount < 1 and not reloading:
 		reloading = true
 		can_shoot = false
 		$Reload.start()
 
+func _fire_burst() -> void:
+	can_shoot = false        # Block new shots immediately
+	currentBulletAmount -= bulletsShot
+
+	# Clamp so it does not go negative
+	if currentBulletAmount < 0:
+		currentBulletAmount = 0
+
+	_burst_shoot()
+
 func _on_timer_timeout() -> void:
-	# Cooldown finished. If not reloading, allow next shot
 	if not reloading:
 		can_shoot = true
 
 func _on_reload_timeout() -> void:
-	# Refill clip after reload
 	currentBulletAmount = bulletAmount
 	reloading = false
 	can_shoot = true
 
-func spawn_bullet():
+# Separate async function for the burst
+func _burst_shoot() -> void:
+	if bulletsShot > 1:
+		for i in range(bulletsShot):
+			spawn_bullet()
+			await get_tree().create_timer(timerSpeed/(bulletsShot/2)).timeout
+	else:
+		spawn_bullet()
+	$coolDown.start()
+
+func spawn_bullet() -> void:
 	var bullet = bulletPrefab.instantiate()
-	bullet.position = global_position
+	bullet.global_position = global_position
 	bullet.look_at(get_global_mouse_position())
 	get_tree().root.get_node("Node2D").add_child(bullet)
