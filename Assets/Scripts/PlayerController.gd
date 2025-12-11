68e33dffa75b48c420a87553eccb31800439de27
Gh0strnx <lucadamico01@gmail.com>
2025-12-11 15:19:23 +1100

    mung us

--- Assets/Scripts/PlayerController.gd
+++ Assets/Scripts/PlayerController.gd
@@ -6,5 +6,19 @@ func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
 	
+	if velocity:
+		$Sprite.play("run")
+		$Sprite2.play("run")
+	else:
+		$Sprite.play("idle")
+		$Sprite2.play("idle")
+		
+	if Input.is_action_just_pressed("left"):
+		$Sprite.flip_h = true
+		$Sprite2.flip_h = true
+	elif Input.is_action_just_pressed("right"):
+		$Sprite.flip_h = false
+		$Sprite2.flip_h = false
+	
 
 	move_and_slide()
