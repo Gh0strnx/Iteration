8fd739a793c26b7b0dae322349b5317d15e1e139
Gh0strnx <lucadamico01@gmail.com>
2026-02-12 12:15:58 +1100

    fixed light stuff

--- Assets/Scripts/bullet.gd
+++ Assets/Scripts/bullet.gd
@@ -3,9 +3,9 @@ extends Area2D
 
 
 @export var damage = 1.0
-@export var speed: float = 720.0
+@export var speed: float = 1050.0
 @export var bullet_bounces: int = 0 
-@export var bulletRange = 100
+@export var bulletRange = 3.5
 
 
 
@@ -16,9 +16,16 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	global_position += dir * speed * delta
+	
+	DropOff()
 
 func _on_body_entered(body):
 
 	if body == GameManager.localPlayer && body.has_method("hurt_player"):
 		body.hurt_player(damage)
 	queue_free()
+
+func DropOff():
+	if bulletRange < 3.5:
+		await get_tree().create_timer(bulletRange).timeout
+		queue_free()
