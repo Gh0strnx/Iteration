558fc91f50f8d3a167a2876abcd5f0fbd72e2e3c
Gh0strnx <lucadamico01@gmail.com>
2026-02-13 07:58:35 +1100

    fixedBullets

--- Assets/Scripts/point_light.gd
+++ Assets/Scripts/point_light.gd
@@ -1,6 +1,5 @@
 extends PointLight2D
 
-@onready var player: Node2D = get_tree().get_first_node_in_group("Player") as Node2D
 @onready var bullet_body: Node2D = get_parent() as Node2D
 @export var waitTimeMult = 1
 
@@ -17,18 +16,18 @@ func _ready() -> void:
 
 func _physics_process(_delta: float) -> void:
 	# Safety: if something is missing, keep the light on
-	if player == null or bullet_body == null:
+	if GameManager.localPlayer == null or bullet_body == null:
 		visible = true
 		return
 
-	var from: Vector2 = player.global_position
+	var from: Vector2 = GameManager.localPlayer.global_position
 	var to: Vector2 = bullet_body.global_position
 
 	var space_state: PhysicsDirectSpaceState2D = get_world_2d().direct_space_state
 	var params: PhysicsRayQueryParameters2D = PhysicsRayQueryParameters2D.create(from, to)
 	params.collide_with_bodies = true
 	params.collide_with_areas = true
-	params.exclude = [player]  # ignore the player itself
+	params.exclude = [GameManager.localPlayer]  # ignore the player itself
 
 	var result: Dictionary = space_state.intersect_ray(params)
 
